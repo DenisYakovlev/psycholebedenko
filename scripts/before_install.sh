dc0282aac83f1178b7aecd08970e3a0e8143e84a
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-07-10 02:47:02 +0300

    fixing appspec.yml

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -21,6 +21,9 @@ echo "installing docker-compose"
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# set docker path
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 echo "docker-compose setup done"
 
 echo "before_install.sh done"
