08f01d4d1c39921bfc54e3e1f8878e252b43067d
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-07-10 05:09:57 +0300

    typo

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@ sudo chmod -R 777 /home/ec2-user/
 cd home/ec2-user/
 
 sudo docker-compose up -d
-sudo docker exec -it -d backend python manage.py runbot
+# sudo docker exec -it -d backend python manage.py runbot
