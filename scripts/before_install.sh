91ec0a4765a87443310f19b333dc0514d32b18da
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-07-10 03:44:27 +0300

    fix scripts..

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,13 +5,13 @@ echo "starting before_install.sh"
 # install docker
 echo "installing docker"
 
-sudo amazon-linux-extras install docker
+sudo yum -y install docker
 sudo service docker start
-sudo usermod -a -G docker ec2-user
+sudo systemctl enable docker
 
-# Enable docker service at boot time
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
+sudo usermod -a -G docker ec2-user
+sudo chmod 666 /var/run/docker.sock
+docker version
 
 echo "docker setup done"
 
