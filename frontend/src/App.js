e8fa93c36c0cf37c6dac8be49c76812122984afe
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-07-12 07:38:42 +0300

    testing routes with ci/cd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Home(){
+  return (
+    <h1>Testing frontend...😼🍇</h1>
+  )
+}
+
+function Event(){
+  return (
+    <h1>Event route...🐔🐗🐘</h1>
+  )
+}
 
 function App() {
   return (
-    <h1>Testing frontend...😼🚬</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Home}/>
+        <Route path="/event" element={null}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
