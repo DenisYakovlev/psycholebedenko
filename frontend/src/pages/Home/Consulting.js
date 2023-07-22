99b1ca7f4bcee7c5cae0ce74b85cf71c879eb809
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-07-23 02:38:51 +0300

    added linear gradient and fixed background issues

--- frontend/src/pages/Home/Consulting.js
+++ frontend/src/pages/Home/Consulting.js
@@ -2,32 +2,20 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Image from "react-bootstrap/Image"
-import bgNormal from "./../../assets/images/bg-new.png"
-// import "./styles.css"
+import bgImage from "./../../assets/images/bg-new.png"
+
+const styles={
+    wrapper: {
+        height: "100vh",
+        backgroundImage: `linear-gradient(rgba(255, 255 ,255 , 0), rgba(0, 0, 0, 1)), url(${bgImage})`,
+        backgroundPosition: "center"
+    }
+}
 
 export default function Consulting(){
-    const screenTreshold = 1200
-
     return (
-        <Container 
-            id="consulting" 
-            fluid
-            style={{height: "100vh"}}
-            className="p-0 position-relative d-flex justify-content-center overflow-hidden"
-        >
-            <Image style={{height: "auto"}} className="position-absolute" src={bgNormal} />
+        <Container style={styles.wrapper} className="p-0 d-flex justify-content-center align-items-center" fluid>
+            
         </Container>
     )
 }
-
-{/* <Container 
-            id="consulting" 
-            fluid
-            style={{height: "100vh"}}
-            className="p-0 position-relative d-flex justify-content-md-center align-items-md-center overflow-hidden"
-        >
-            {window.screen.width > screenTreshold ?
-                <Image className="position-absolute" src={bgImage1}/> :
-                <Image style={{height: "auto", width: "100%", minWidth: "580px"}} className="position-absolute" src={bgImage2} fluid/>
-            }
-        </Container> */}
