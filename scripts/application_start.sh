6c06c3695964bd3a87dc5b4faed8637787b27fc2
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-08-07 23:09:01 +0300

    added extra time for app start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,8 +4,8 @@ sudo chmod -R 777 /home/ec2-user/
 cd home/ec2-user/
 
 echo "building app"
-sudo docker-compose build
+# sudo docker-compose build
 
 echo "starting app"
-sudo docker-compose up -d
+sudo docker-compose up -d --build	
 sudo docker exec -it -d backend python manage.py runbot
