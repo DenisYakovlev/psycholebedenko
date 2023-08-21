6d74e42fd060963211f271758908e988a5c45c55
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-08-21 18:08:58 +0300

    creating schedule model for admin panel

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -3,4 +3,8 @@ from .models import TelegramUser
 
 # Register your models here.
 
-admin.site.register(TelegramUser)
+# admin.site.register(TelegramUser)
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'phone_number')
