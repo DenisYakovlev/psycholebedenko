601f498edd963adcf030710dcd872c881f17eafa
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-09-07 09:23:10 +0300

    error fix

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -4,5 +4,5 @@ echo "stopping docker-compose"
 
 cd home/ec2-user/
 
-sudo docker-compose stop
-sudo docker-compose down
+sudo docker-compose -f docker-compose.prod.yml stop
+sudo docker-compose -f docker-compose.prod.yml down
