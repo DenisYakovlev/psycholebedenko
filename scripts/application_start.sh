23a6c1b510cfc0be5f49a9869b6b4af17b63c1e3
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-09-22 01:43:31 +0300

    added telegram bot container and removed manual bot start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,8 +4,9 @@ sudo chmod -R 777 /home/ec2-user/
 cd home/ec2-user/
 
 echo "building app"
-# sudo docker-compose build
 
 echo "starting app"
 sudo docker-compose -f docker-compose.prod.yml up -d --build	
-sudo docker exec -it -d backend python manage.py runbot
+
+# manual bot start
+# sudo docker exec -it -d backend python manage.py runbot
