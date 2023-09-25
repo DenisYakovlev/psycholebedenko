7e15bd9e29c84a1426d825ea01c6e02dd1d20d2c
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-09-25 14:46:17 +0300

    added pagination and filters

---
+++ backend/appointment/filters.py
@@ -0,0 +1,35 @@
+from django_filters import rest_framework as filters
+import pytz
+from datetime import datetime
+from django.conf import settings
+
+
+class AppointmentFilters(filters.FilterSet):
+    user = filters.CharFilter(method='filter_by_user', field_name='user')
+    status = filters.CharFilter(method='filter_by_status', field_name='status')
+    outdated = filters.BooleanFilter(method='filter_by_date', field_name='date__date')
+
+    def filter_by_user(self, queryset, field, value):
+        try:
+            return queryset.filter(user=value)
+        
+        except queryset.DoesNotExist:
+            return queryset
+        
+
+    def filter_by_status(self, queryset, field, value):
+        try:
+            _status = value.split(',')
+            return queryset.filter(status__in=_status)
+        
+        except:
+            return queryset
+        
+    def filter_by_date(self, queryset, field, value):
+        tz = pytz.timezone(settings.TIME_ZONE)
+        now = datetime.now(tz=tz)
+
+        if value:
+            return queryset
+        else:
+            return queryset.filter(date__date__gt=now)
--- backend/appointment/views.py
+++ backend/appointment/views.py
@@ -10,64 +10,37 @@ from django.conf import settings
 from django.http import Http404
 
 from .models import Appointment
+from .paginations import AppointmentPagination
+from .filters import AppointmentFilters
 from .serializers import AppointmentSerializer, AppointmentListSerializer, AppointmentCreateSerializer
 from user.models import TelegramUser
 from schedule.models import Schedule
 
 
-class AppointmentList(APIView):
+class AppointmentList(generics.ListCreateAPIView):
     permission_classes = [IsAdminUser]
+    pagination_class = AppointmentPagination
+    filterset_class = AppointmentFilters
+    queryset = Appointment.objects.order_by('date__date').all()
     
-    def get_queryset(self):
-        queryset = Appointment.objects.order_by('date__date').all()
+    def get_serializer_class(self):
+        serializers = {
+            "GET": AppointmentListSerializer,
+            "POST": AppointmentCreateSerializer
+        }
 
-        try:
-            if 'user' in self.request.query_params:
-                _user = self.request.query_params.get('user')
-                queryset = queryset.filter(user=_user)
-
-            if 'status' in self.request.query_params:
-                _status = self.request.query_params.get('status').split(',')
-                queryset = queryset.filter(status__in=_status)
-
-            if 'outdated' in self.request.query_params:
-                _outdated = self.request.query_params.get('outdated') == "1"
-                tz = pytz.timezone(settings.TIME_ZONE)
-                now = datetime.now(tz=tz)
-
-                if not _outdated:
-                    queryset = queryset.filter(date__date__gt=now)
-
-            return queryset
-        except:
-            return queryset
-
-    
-    def get(self, request):
-        appointments = self.get_queryset()
-        serializer = AppointmentListSerializer(appointments, many=True)
-        
-        return Response(serializer.data, status.HTTP_200_OK)
+        return serializers[self.request.method]
     
     def post(self, request):
         try:
-            # user_phone = request.data["user"]
-            # user = TelegramUser.objects.filter(phone_number=user_phone).first()
-            # request.data['user'] = user
-
             # only 10 pending appointments are allowed
             if Appointment.objects.filter(user=request.data["user"], status=Appointment.Status.PENDING).count() > 10:
                 return Response({"msg": "Pending appointments limit exceeded"}, status.HTTP_409_CONFLICT)
-        except:
+            
+        except Appointment.DoesNotExist:
             return Response({"msg": "user not found"}, status.HTTP_404_NOT_FOUND)
-
-        serializer = AppointmentCreateSerializer(data=request.data, context={'request': request})
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
         
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        return super().post(request)
     
 
 class AppointmentCreate(APIView):
