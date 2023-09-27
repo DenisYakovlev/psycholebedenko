72a12b8a33899229430f4842bb3ee8c7a0798380
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-09-27 18:23:43 +0300

    added exit for bot client testing

--- frontend/src/pages/Home/Consulting/ConsultingButton.js
+++ frontend/src/pages/Home/Consulting/ConsultingButton.js
@@ -2,9 +2,19 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button"
 import { Link } from "react-router-dom";
 import "./styles.css"
+import { useContext } from "react";
+import { UserContext } from "../../../contexts";
 
 
 export default function ConsultingButton({source}){
+    const {setUser} = useContext(UserContext)
+
+    const handleExit = () => {
+        localStorage.removeItem('tokens')
+        setUser(null)
+        alert("exited")
+    }
+
     return (
         <Container className="m-0 pb-5 d-flex flex-column align-items-center justify-content-end gap-3 main-button-wrapper">
             <Button 
@@ -14,8 +24,8 @@ export default function ConsultingButton({source}){
             >
                 ЗАПИСАТИСЯ
             </Button>
-            {/* <p style={{fontSize: "8px"}} >
-                какой-то текст под кнопкой
+            {/* <p onClick={handleExit} style={{fontSize: "12px"}} >
+                exit
             </p> */}
         </Container>
     )
