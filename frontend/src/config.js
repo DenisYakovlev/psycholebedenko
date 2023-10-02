10503b9cbbbe1ecc31720a78d2454fb863b0de3c
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-10-02 19:44:54 +0300

    added config for tests

---
+++ frontend/src/config.js
@@ -0,0 +1,16 @@
+// write config for testometrica tests here
+const testsConfig = [
+    {
+        name: "Тест на рівень депресії",
+        id: "qclnwigf",
+        img: "https://psycholebedenko-backend.s3.amazonaws.com/test-card-img.png"
+    },
+    {
+        name: "Ваша формула темперамента",
+        id: "noysvtzg",
+        img: "https://psycholebedenko-backend.s3.amazonaws.com/test-card-img.png"
+    }
+]
+
+
+export {testsConfig}
