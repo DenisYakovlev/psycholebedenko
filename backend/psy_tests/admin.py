618f197f9ce4163bd47346e0344d0157bea5f8ce
DenisYakovlev <denis.yakovlev098@gmail.com>
2023-10-23 22:03:46 +0300

    changed admin site settings for psy tests

--- backend/psy_tests/admin.py
+++ backend/psy_tests/admin.py
@@ -3,6 +3,11 @@ from .models import PsycholyTest, TestResult, TestAnswer
 
 # Register your models here.
 
-admin.site.register(PsycholyTest)
+
+@admin.register(PsycholyTest)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('name')
+    
+
 admin.site.register(TestAnswer)
 admin.site.register(TestResult)
