719044ed3cfda152c181b8fd40a436cd4ee94a0a
DenisYakovlev <denis.yakovlev098@gmail.com>
2024-02-11 03:05:34 +0200

    added debounce constant

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,5 @@
 const backend_url = process.env.REACT_APP_BACKEND_URL 
 const backend_ws_url = process.env.REACT_APP_BACKEND_WS_URL
+const debounce_delay_ms = 300
 
-export {backend_url, backend_ws_url}
+export {backend_url, backend_ws_url, debounce_delay_ms}
