5614b10caabef792dbf03660ed618a679d97181b
DenisYakovlev <denis.yakovlev098@gmail.com>
2024-06-20 16:08:50 +0300

    improved bot page for manual tg WebApp auth testing

--- frontend/src/pages/Bot/Bot.js
+++ frontend/src/pages/Bot/Bot.js
@@ -1,13 +1,24 @@
 import { useContext } from "react"
 import { UserContext } from "../../contexts"
 
+// this page is only for testing authentication from tg web app.
 export default function Bot(){
+
     // var tg = window.Telegram.WebApp
-    const {user} = useContext(UserContext)
+    const {user, setUser} = useContext(UserContext)
+
+    const handleExit = () => {
+        localStorage.removeItem('tokens')
+        setUser(null)
+    }
 
     return (
-        <p className="m-0 p-0 text-break fs-6 text-dark">
+        <p className="m-0 p-0 text-break fs-6 text-dark d-flex flex-column gap-3">
+            <h1 onClick={handleExit} className="m-0 p-0">
+                Вийти
+            </h1>
             {JSON.stringify(user)}
+            {JSON.stringify(window.Telegram.WebApp)}
         </p>
     )
 }
