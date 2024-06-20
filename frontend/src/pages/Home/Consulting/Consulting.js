4f66e746ea470ec393d30caa0d6fe66df00d58b4
DenisYakovlev <denis.yakovlev098@gmail.com>
2024-06-20 16:09:24 +0300

    changed height of Consutling section of Home page

--- frontend/src/pages/Home/Consulting/Consulting.js
+++ frontend/src/pages/Home/Consulting/Consulting.js
@@ -30,7 +30,8 @@ export default function Consulting({source=""}){
                 backgroundSize: "cover",
                 backgroundPosition: "center",
                 fontFamily: ["Cousine", "Courier"],
-                minHeight: "calc(100vh - var(--nav-height))",
+                // minHeight: "calc(100vh - var(--nav-height))",
+                minHeight: "100vh",
                 height: "fit-content"
             }} className="m-0 p-0 d-flex align-items-end justify-content-center" fluid>
                 <Row className="m-0 p-0 mt-5">
