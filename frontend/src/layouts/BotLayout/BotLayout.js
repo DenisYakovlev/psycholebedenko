87dc96ce9d29ae30f308a4d5893235942cddb7a8
DenisYakovlev <denis.yakovlev098@gmail.com>
2024-08-07 20:47:41 +0300

    added alerts for bot auth testing

--- frontend/src/layouts/BotLayout/BotLayout.js
+++ frontend/src/layouts/BotLayout/BotLayout.js
@@ -1,120 +1,120 @@
-import { useContext, useEffect, useState } from "react"
-import { Outlet } from "react-router"
-import { useNavigate } from "react-router"
-import Container from "react-bootstrap/Container"
-import { LoadSpinner } from "../../shared"
-import NavBar from "./NavBar"
-import useApi from "../../hooks/useApi"
-import './styles.css'
-import { UserContext } from "../../contexts"
-import { backend_url } from "../../constants"
+import { useContext, useEffect, useState } from "react";
+import { Outlet } from "react-router";
+import { useNavigate } from "react-router";
+import Container from "react-bootstrap/Container";
+import { LoadSpinner } from "../../shared";
+import NavBar from "./NavBar";
+import useApi from "../../hooks/useApi";
+import "./styles.css";
+import { UserContext } from "../../contexts";
+import { backend_url } from "../../constants";
 
+export default function BotLayout() {
+  let navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const { verifyToken, refreshToken } = useApi();
 
-export default function BotLayout(){
-    let navigate = useNavigate()
-    const {user, setUser} = useContext(UserContext)
-    const [isLoading, setIsLoading] = useState(false)
-    const { verifyToken, refreshToken} = useApi()
+  useEffect(() => {
+    const refresh = async () => {
+      setIsLoading(true);
+      const tokenIsValid = await verifyToken();
 
+      if (!tokenIsValid) {
+        await refreshToken();
+      }
 
-    useEffect(() => {
-        const refresh = async () => {
-            setIsLoading(true)
-            const tokenIsValid = await verifyToken()
+      setIsLoading(false);
+    };
 
-            if(!tokenIsValid){
-                await refreshToken()
-            }
+    const authorizeUser = async () => {
+      setIsLoading(true);
 
-            setIsLoading(false)
-        }
+      await fetch(`${backend_url}/auth/telegram/botapp`, {
+        method: "POST",
+        headers: {
+          "Content-type": "Application/json",
+        },
+        body: JSON.stringify({
+          initData: window.Telegram.WebApp.initData,
+        }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
 
-        const authorizeUser = async () => {
-            setIsLoading(true)
+          throw new Error("Bot user auth error");
+        })
+        .then((data) => {
+          localStorage.setItem("tokens", JSON.stringify(data));
+          setUser(data);
+        })
+        .catch((error) => alert(error.toString()));
 
-            await fetch(`${backend_url}/auth/telegram/botapp`, {
-                method: "POST",
-                headers: {
-                    "Content-type": "Application/json"
-                },
-                body: JSON.stringify({
-                    initData: window.Telegram.WebApp.initData
-                })
-            })
-            .then(response => {
-                if(response.ok){
-                    return response.json()
-                }
+      setIsLoading(false);
+    };
 
-                throw new Error("Bot user auth error")
-            })
-            .then(data => {
-                localStorage.setItem('tokens', JSON.stringify(data))
-                setUser(data)
-            })
-            .catch(error => alert(error.toString()))
+    const seamlessAuthorize = async (body) => {
+      setIsLoading(true);
 
-            setIsLoading(false)
-        }
+      await fetch(`${backend_url}/auth/telegram/seamless`, {
+        method: "POST",
+        headers: {
+          "Content-type": "Application/json",
+        },
+        body: JSON.stringify(body),
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
 
-        const seamlessAuthorize = async (body) => {
-            setIsLoading(true)
+          throw new Error("Bot seamless auth error");
+        })
+        .then((data) => {
+          localStorage.setItem("tokens", JSON.stringify(data));
+          setUser(data);
+        })
+        .catch((error) => alert(error));
 
-            await fetch(`${backend_url}/auth/telegram/seamless`, {
-                method: "POST",
-                headers: {
-                    "Content-type": "Application/json"
-                },
-                body: JSON.stringify(body)
-            })
-            .then(response => {
-                if(response.ok){
-                    return response.json()
-                }
+      setIsLoading(false);
+    };
 
-                throw new Error("Bot seamless auth error")
-            })
-            .then(data => {
-                localStorage.setItem('tokens', JSON.stringify(data))
-                setUser(data)
-            })
-            .catch(error => alert(error))
+    // for testing only
+    alert(window?.Telegram?.WebApp?.initData);
+    alert(JSON.stringify(user));
 
-            setIsLoading(false)
-        }
+    if (window.Telegram.WebApp.initData.length == 0) {
+      const params = new URLSearchParams(window.location.search);
 
-        if(window.Telegram.WebApp.initData.length == 0){
-            const params = new URLSearchParams(window.location.search)
+      if (params.get("hash") && params.get("id")) {
+        seamlessAuthorize({ id: params.get("id"), hash: params.get("hash") });
+        return;
+      } else {
+        navigate("/");
+        return;
+      }
+    }
 
-            if(params.get("hash") && params.get("id")){
-                seamlessAuthorize({id: params.get("id"), hash: params.get("hash")})
-                return
-            }
-            else{
-                navigate('/')
-                return
-            }
-        }
+    if (!user) {
+      authorizeUser();
+      return;
+    } else {
+      refresh();
+    }
+  }, []);
 
-        if(!user){
-            authorizeUser()
-            return
-        }
-        else{
-            refresh()
-        }
-    }, [])
-
-    return (
-        <Container className="p-0" fluid>
-            {isLoading ?
-                <LoadSpinner />
-                :
-                <>
-                    <NavBar />
-                    <Outlet />
-                </>
-            }
-        </Container>
-    )
+  return (
+    <Container className="p-0" fluid>
+      {isLoading ? (
+        <LoadSpinner />
+      ) : (
+        <>
+          <NavBar />
+          <Outlet />
+        </>
+      )}
+    </Container>
+  );
 }
